9566483b6de1965227e3d5a12e4d88ff6fa6a421
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2026-04-10 12:08:01 +0300

    Add all funcs in SingleLinkedStack.kt

--- src/main/kotlin/stack/SingleLinkedStack.kt
+++ src/main/kotlin/stack/SingleLinkedStack.kt
@@ -1,6 +1,30 @@
 package org.example.stack
 
+import org.example.list.SingleLinkedList
+import org.example.Stack
 
-class SingleLinkedStack : Stack {
-    // TODO: have to inherit from SingleLinkedStack! Add inheritance before implementation!
+class SingleLinkedStack : SingleLinkedList(), Stack {
+
+    override fun push(value: Int) {
+        addFirst(value)
+    }
+
+    override fun pop(): Int {
+        if (isEmpty) {
+            throw NoSuchElementException("Error: Stack is empty")
+        }
+        val value = get(0)
+        remove(value)
+        return value
+    }
+
+    override fun peek(): Int {
+        if (isEmpty) {
+            throw NoSuchElementException("Error: Stack is empty")
+        }
+        return get(0)
+    }
+
+    override val isEmpty: Boolean
+        get() = size == 0
 }
