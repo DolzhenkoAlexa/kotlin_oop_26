e7863be658afc8c38cc4b1ec774e34f2ea4c1c03
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2026-04-10 12:04:51 +0300

    Add print in ListPrinter.kt

--- src/main/kotlin/ListPrinter.kt
+++ src/main/kotlin/ListPrinter.kt
@@ -3,8 +3,16 @@ package org.example
 import org.example.list.CustomArrayList
 import org.example.list.CustomList
 
-object ListPrinter {
-//    fun printList(list: ????) {
-//        implement
-//    }
+class ListPrinter {
+    fun printList(collection: CustomList) {
+        print("Printing:")
+        print("[")
+        for (i in 0 until collection.size) {
+            if (i > 0) {
+                print(", ")
+            }
+            print(collection[i])
+        }
+        println("]")
+    }
 }
